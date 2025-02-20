185a94fbe613811f692b7df57095b4820ff3cadd
ErbKqlx <Error5455@yandex.ru>
2025-02-20 13:10:24 +0500

    Использование фабрики для создания модели представления

--- app/src/main/java/com/example/guessinggame/ResultFragment.kt
+++ app/src/main/java/com/example/guessinggame/ResultFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.guessinggame.databinding.FragmentResultBinding
 
@@ -17,6 +18,9 @@ class ResultFragment : Fragment() {
     private var _binding: FragmentResultBinding? = null
     private val binding get() = _binding!!
 
+    lateinit var viewModel: ResultViewModel
+    lateinit var viewModelFactory: ResultViewModelFactory
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,7 +28,11 @@ class ResultFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
-        binding.wonLost.text = ResultFragmentArgs.fromBundle(requireArguments()).result
+        val result = ResultFragmentArgs.fromBundle(requireArguments()).result
+        viewModelFactory = ResultViewModelFactory(result)
+        viewModel = ViewModelProvider(this, viewModelFactory)
+                                    .get(ResultViewModel::class.java)
+        binding.wonLost.text = viewModel.result
         binding.newGameButton.setOnClickListener {
             view.findNavController()
                 .navigate(R.id.action_resultFragment_to_gameFragment)
